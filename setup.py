49f7e93772a16ffefe56f4a29d5c2c9fc1dcbb84
Antoine Boucher <antoine.bou13@gmail.com>
2024-01-26 16:01:18 -0500

    Update setup.py (#140)
    * Update setup.py
    
    * Update pyproject.toml (#141)

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ README = (HERE / "README.md").read_text()
 # This call to setup() does all the work
 setup(
     name="marketwatch",
-    version="0.2.1",
+    version="0.2.2",
     description="MarketWatch's API",
     long_description=README,
     long_description_content_type="text/markdown",
